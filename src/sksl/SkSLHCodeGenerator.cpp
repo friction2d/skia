ff8e4fdd8e60af2a6aa7a5eabdc5b362236b6ad2
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2025-03-21 11:35:47 +0100

    vsprintf => vsnprintf

--- src/sksl/SkSLHCodeGenerator.cpp
+++ src/sksl/SkSLHCodeGenerator.cpp
@@ -109,7 +109,7 @@ void HCodeGenerator::writef(const char* s, va_list va) {
         fOut->write(buffer, length);
     } else {
         std::unique_ptr<char[]> heap(new char[length + 1]);
-        vsprintf(heap.get(), s, copy);
+        vsnprintf(heap.get(), s, copy);
         fOut->write(heap.get(), length);
     }
     va_end(copy);
